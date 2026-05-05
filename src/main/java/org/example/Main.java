146c15acf563acca393928606420b63d9b9c641d
DESKTOP-1QBU77R\Ricardo <ricardo.rodrigs@outlook.com>
2026-05-04 23:02:26 -0300

    implementação inicial do Spring Boot

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,23 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * IMPORTANT:
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+@SpringBootApplication
+@EnableAutoConfiguration
 public class Main {
+
     public static void main(String[] args) {
 
-            System.out.printf("Lobinho, Lobinho AUUUUUUUUUUUUUUUUUUUUUUU!");
+        SpringApplication.run(Main.class,args);
 
     }
+
 }
